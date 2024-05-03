0cd3a08b4897f1a8cd2674a64825120c4f5c4c07
tjugosex <mikael.skaremyr.te1@hotmail.com>
2024-05-03 10:08:18 +0200

    Added buttons and code to interact with the canvas

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.externallibraries;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.luciferx86.doodlecanvaslibrary.DoodleCanvas;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    DoodleCanvas canvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        canvas = findViewById(R.id.doodleCanvas);
+        Button redButton = findViewById(R.id.redButton);
+        Button blackButton = findViewById(R.id.blackButton);
+        Button clearButton = findViewById(R.id.clearButton);
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                canvas.setStrokeColor(Color.RED);
+            }
+        });
+
+        blackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                canvas.setStrokeColor(Color.BLACK);
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                canvas.clearCanvas();
+            }
+        });
     }
+
 }
